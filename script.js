7c4404c9d078f1e363b4c7ba3cd087c7c967c6e2
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-20 11:45:45 -0500

    imported actual images and added them to the html
    since the images will always be in view I simplified the function to just add the startTilt class

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const tiltImages = document.querySelectorAll('.sarah, .amy, .lurie');
-const windowHeight = window.innerHeight;
-console.log('were gonna tilt!')
-function checkPosition() {
+
+function startTilt() {
     for (let img of tiltImages) {
-        const topOfElement = img.getBoundingClientRect().top;
-        console.log('tilting')
-        // Check if the image is in view otherwise the animation stops
-        if (topOfElement < windowHeight && topOfElement + img.clientHeight > 0) {
-            img.classList.add('startTilt');
-        } else {
-            img.classList.remove('startTilt');
-        }
+        img.classList.add('startTilt');      
+       
     }
 }
 
 
-checkPosition();
+startTilt();
