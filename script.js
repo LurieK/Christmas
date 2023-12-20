4d1bf6f37b5f1e6e05b565522d54885f50ded324
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-20 11:16:12 -0500

    set width and bottom margins
    add script for tilting
    add styling for tilting

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const tiltImages = document.querySelector('.sarah, .amy, .lurie');
-
-function startTilte(){
-    for(let img of tiltImages){
-        img.classList.add('startTilt');
-
+const tiltImages = document.querySelectorAll('.sarah, .amy, .lurie');
+const windowHeight = window.innerHeight;
+console.log('were gonna tilt!')
+function checkPosition() {
+    for (let img of tiltImages) {
+        const topOfElement = img.getBoundingClientRect().top;
+        console.log('tilting')
+        // Check if the image is in view otherwise the animation stops
+        if (topOfElement < windowHeight && topOfElement + img.clientHeight > 0) {
+            img.classList.add('startTilt');
+        } else {
+            img.classList.remove('startTilt');
+        }
     }
 }
+
+
+checkPosition();
